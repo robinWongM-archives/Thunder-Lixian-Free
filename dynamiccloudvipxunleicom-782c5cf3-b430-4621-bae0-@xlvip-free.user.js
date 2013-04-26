5d75dd2d9cdba6a0575bd30f65bfa45bef95bf1d
m208 <1010336298@qq.com>
2013-04-26 18:42:35 +0800

    更正错误。

--- dynamiccloudvipxunleicom-782c5cf3-b430-4621-bae0-@xlvip-free.user.js
+++ dynamiccloudvipxunleicom-782c5cf3-b430-4621-bae0-@xlvip-free.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             dynamic.cloud.vip.xunlei.com-782c5cf3-b430-4621-bae0-0e90d9fc365d@xlvip-free
 // @name           迅雷离线免费版
-// @version        1.1.1
+// @version        1.1.2
 // @namespace      xlvip-free
 // @author         m208
 // @description    迅雷离线免费版，为领取了六个月免费资格的雷友提供高速取回服务。
@@ -21,7 +21,7 @@ var main = function () {
 	try{$('#li_task_down span em').html('取回本地');}catch(e){};
 	try{$('#li_task_down').attr('title','取回本地（Powered by 迅雷离线免费版）');}catch(e){};
 	$('.kj_box div').text('您是迅雷免费版用户，可以体验会员加速功能');
-	$('.say_side').prepend('<a class="btn_m" title="Chrome用户点此检查更新（迅雷离线免费版）" target="_blank" href="https://github.com/m208/Thunder-Lixian-Free"><span><em class="ic_sayfeel">关于迅雷离线免费版</em></span></a>');
+	$('.say_side').prepend('<a class="btn_m" title="点击打开迅雷离线免费版网站" target="_blank" href="https://github.com/m208/Thunder-Lixian-Free"><span><em class="ic_sayfeel">关于迅雷离线免费版</em></span></a>');
 };
 
 // Inject our main script
